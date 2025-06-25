f25aaef0750600733529c9e64f6083dceb9a6d00
Rahul Prasad <rahulprasad1399@gmail.com>
2025-06-25 15:34:20 +0530

    changed the name of file from default.js to page.js

--- data mutation/app/new-post/page.js
+++ data mutation/app/new-post/page.js
@@ -1,68 +1,8 @@
-"use client";
-import FormSubmit from '@/components/form-submit';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
-import { useFormState } from 'react-dom';
+import PostForm from '@/components/post-form';
+import { createPost } from '@/actions/posts';
 
 export default function NewPostPage() {
-  const [state, formAction] =  useFormState(createPost, {})
-  
-  async function createPost(formData){
-    "use server"
-    const title = formData.get('title')
-    const image = formData.get('image')
-    const content = formData.get('content')
 
-    let errors = []
-    if(!title ||  title.trim().length === 0){
-      errors.push("Title is required")
-    }
-    if(!content || content.trim().length === 0){
-      errors.push("content is required")
-    }
-    if(!image){
-      errors.push("image is required")
-    }
+  return <PostForm action={createPost} />
 
-    if(errors.length > 0){
-      return { errors }
-    }
-
-    await storePost({
-      imageUrl : '',
-      title,
-      content,
-      userId : 1
-    })
-
-    redirect('/feed')
-  }
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form> 
-    </>
-  );
 }
